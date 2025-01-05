fd86a38aa79ea6ccd4e593f37e7caf0fc2c836d5
Eknath Reddy M <151753511+eknaathreddyM@users.noreply.github.com>
2025-01-05 15:29:51 +0530

    Delete PaymentGateway directory
    unnessary files

--- PaymentGateway/UserService/src/main/java/com/example/Repo/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.Repo;
-
-import com.example.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo  extends JpaRepository<User, Long> {
-}
--- PaymentGateway/WalletService/src/main/java/com/example/Repo/WalletRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.Repo;
-
-import com.example.entity.Wallet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WalletRepo extends JpaRepository<Wallet, Long> {
-
-    Wallet findByUserId(Long id);
-}
